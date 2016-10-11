b0b254c5d694af30b5e053554c824e6d25a63eae
Jakob Riga <starwarsjunkie@freenet.de>
2016-10-11 02:51:31 +0200

    Initial Commit

---
+++ HomeTransferSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HomeTransferSwift
+//
+//  Created by Jakob Riga on 11.10.16.
+//  Copyright © 2016 Jakob Riga. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
